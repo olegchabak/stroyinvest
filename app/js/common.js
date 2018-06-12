7ed461c47b93316728013a1a39d382bd36a3faa4
Oleg Chabak <cheps@list.ru>
2018-06-12 19:09:25 +0300

    Доделал главную для десктопа, начал адаптив

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,26 @@
 $(function() {
+	
+	// ******* Button mobile menu *******
+	$('#topNavBtn').on('click', function() {
+		$('.hamburger').toggleClass('is-active');
+		$('nav').slideToggle();
+	});
+	// ******* Button left-side mobile category *******
+	$('#leftSidebarBtn').on('click', function() {
+		$('.left-sidebar_menu').slideToggle();
+	});
+	
 
-	// Custom JS
+
+	// ******** Slick slider ********
+	$('.directions-blocks').slick({
+		slidesToShow: 4,
+		autoplay: true,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		arrows: false,
+		dots: true
+	});
+
 
 });
